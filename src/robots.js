72cc8e9d4a585c7109d8ade14db13a231b23db65
Tewodros-Mekonnen <teo.meko@gmail.com>
2019-10-31 02:52:48 -0500

    added Cards.js, robots.js and edited these files!

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots=[
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'sincere@april.biz'
+	},
+	{
+		id: 2,
+		name: 'Ervin Howell',
+		username: 'Antonette',
+		email:'shann@melissa.tv'
+	},
+	{
+		id: 3,
+		name: 'Clementine Bauch',
+		username: 'samantha',
+		email: 'Nathan@yesenia.net'
+	}
+]
