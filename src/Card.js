7151e39a02ec99cf14fa2e9519895bd94d261316
Tewodros-Mekonnen <teo.meko@gmail.com>
2019-10-31 12:38:53 -0500

    rearranged Card parameters in Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Card=({name,email,id})=>{
+const Card=({id,name,email})=>{
 	return(
 		<div className="bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5 ">
 			<img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
