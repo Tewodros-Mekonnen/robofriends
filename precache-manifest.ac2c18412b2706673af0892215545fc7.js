dab9f38b42452885da94f3f7798ca18ca444b6c2
Tewodros-Mekonnen <teo.meko@gmail.com>
2019-11-12 15:33:22 -0600

    Updates

---
+++ precache-manifest.ac2c18412b2706673af0892215545fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a02d449423c5b4020a235c72761d0b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6b336c0a54ff84be91c3",
+    "url": "/robofriends/static/css/main.bc11e015.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "6b336c0a54ff84be91c3",
+    "url": "/robofriends/static/js/main.93d8aa71.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "3eb2be46a75f69a75ac6142a350c062c",
+    "url": "/robofriends/static/media/Patchy Robots.3eb2be46.woff"
+  }
+]);
